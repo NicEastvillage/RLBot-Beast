65926c0c973d55ae2f481d8dfa6159d4693cd5ee
NicEastvillage <nicoesterby@gmail.com>
2018-06-16 11:17:53 +0200

    Created the Beast, separated files, and setup rlbot.cfg

---
+++ beastbot/rlmath.py
@@ -0,0 +1,22 @@
+import math
+from vec2 import Vec2 
+
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+
+
+def get_car_facing_vector(car):
+    pitch = float(car.physics.rotation.pitch)
+    yaw = float(car.physics.rotation.yaw)
+
+    facing_x = math.cos(pitch) * math.cos(yaw)
+    facing_y = math.cos(pitch) * math.sin(yaw)
+
+    return Vec2(facing_x, facing_y)
+    
+def steer_correction_smooth(val):
+    # increasing the constant will make the correction more smooth
+    smoothness = 0.15
+    val = (1 + smoothness) * val / (smoothness + abs(val))
+    val = min(max(-1, val), 1)
+    
+    return val
---
+++ beastbot/vec2.py
@@ -0,0 +1,27 @@
+import math
+
+class Vec2:
+    def __init__(self, x=0, y=0):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __add__(self, val):
+        return Vec2(self.x + val.x, self.y + val.y)
+
+    def __sub__(self, val):
+        return Vec2(self.x - val.x, self.y - val.y)
+
+    def correction_to(self, ideal):
+        current_in_radians = math.atan2(self.y, self.x)
+        ideal_in_radians = math.atan2(ideal.y, ideal.x)
+
+        correction = ideal_in_radians - current_in_radians
+
+        # Make sure we go the 'short way'
+        if abs(correction) > math.pi:
+            if correction < 0:
+                correction += 2 * math.pi
+            else:
+                correction -= 2 * math.pi
+
+        return correction
