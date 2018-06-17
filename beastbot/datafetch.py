2321dd2dad3c6ac14d61fc462215c33cb065823c
NicEastvillage <nicoesterby@gmail.com>
2018-06-17 20:37:51 +0200

    Added more datafetchers.

--- beastbot/datafetch.py
+++ beastbot/datafetch.py
@@ -1,5 +1,6 @@
 import math
 from vec import Vec3
+import situation
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 # Vec3
@@ -14,3 +15,31 @@ def enemy_location(car, packet: GameTickPacket):
 # Vec3
 def ball_location(car, packet: GameTickPacket):
 	return Vec3(packet.game_ball.physics.location.x, packet.game_ball.physics.location.y)
+	
+# Zone
+def my_half_zone(car, packet: GameTickPacket):
+	if car.team == 0:
+		return situation.BLUE_HALF_ZONE
+	else:
+		return situation.ORANGE_HALF_ZONE
+
+# Zone
+def enemy_half_zone(car, packet: GameTickPacket):
+	if car.team == 1:
+		return situation.BLUE_HALF_ZONE
+	else:
+		return situation.ORANGE_HALF_ZONE
+
+# Vec3
+def my_goal_location(car, packet: GameTickPacket):
+	goal_offset = situation.ARENA_LENGTH - 200
+	if car.team == 0:
+		goal_offset *= -1
+	return Vec3(0, goal_offset)
+
+# Vec3
+def enemy_goal_location(car, packet: GameTickPacket):
+	goal_offset = situation.ARENA_LENGTH - 200
+	if car.team == 1:
+		goal_offset *= -1
+	return Vec3(0, goal_offset)
