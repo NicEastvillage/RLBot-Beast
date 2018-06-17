f879a509429fda387773fa3b19617313f4ad5518
NicEastvillage <nicoesterby@gmail.com>
2018-06-17 20:49:46 +0200

    Added GuardIsPointInZone and GuardIsPointBelowZ.

--- beastbot/nodes.py
+++ beastbot/nodes.py
@@ -1,5 +1,5 @@
 import math
-from vec import Vec3
+from vec import Vec3,Zone
 import behaviourtree as bt
 import moves
 
@@ -17,7 +17,7 @@ class TaskGoTo(bt.BTNode):
 		controller = moves.go_to_point(car, packet, point, slide=True)
 		return (bt.ACTION, self.parent, controller)
 
-# sig: <dist:float> <pointA:Vec3Func> <pointB:Vec3Func>	
+# sig: <dist:float!> <pointA:Vec3Func> <pointB:Vec3Func>	
 class GuardDistanceLessThan(bt.BTNode):
 	def __init(self, arguments):
 		super().__init__()
@@ -30,6 +30,37 @@ class GuardDistanceLessThan(bt.BTNode):
 		pointB = self.pointBFunc(car, packet)
 		
 		if pointA.dist2(pointB) < self.dist * self.dist:
+			return (bt.SUCCESS, self.parent, None)
+		else:
+			return (bt.FAILURE, self.parent, None)
+
+# sig: <point:Vec3> <zone:Zone>
+class GuardIsPointInZone(bt.BTNode):
+	def __init(self, arguments):
+		super().__init__()
+		self.pointFunc = arguments[0]
+		self.zoneFunc = arguments[1]
+	
+	def resolve(self, prev_status, car, packet: GameTickPacket):
+		point = self.pointFunc(car, packet)
+		zone = self.zoneFunc(car, packet)
+		
+		if zone.contains(point):
+			return (bt.SUCCESS, self.parent, None)
+		else:
+			return (bt.FAILURE, self.parent, None)
+
+# sig: <point:Vec3> <z:float!>
+class GuardIsPointBelowZ(bt.BTNode):
+	def __init(self, arguments):
+		super().__init__()
+		self.pointFunc = arguments[0]
+		self.z = arguments[1]
+	
+	def resolve(self, prev_status, car, packet: GameTickPacket):
+		point = self.pointFunc(car, packet)
+		
+		if point.z <= self.z:
 			return (bt.SUCCESS, self.parent, None)
 		else:
 			return (bt.FAILURE, self.parent, None)
