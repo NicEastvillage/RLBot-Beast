7162cdc8e910b62fe3ed5be9a44089cd2e3df8df
NicEastvillage <nicoesterby@gmail.com>
2018-06-23 10:21:20 +0200

    Added some easing functions.

--- beastbot/beastbot.py
+++ beastbot/beastbot.py
@@ -2,9 +2,6 @@ import math
 import rlmath
 from vec import Vec3
 import moves
-import tasks as task
-import guards as guard
-import behaviourtree as bt
 import datafetch
 
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
@@ -14,44 +11,9 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 class Beast(BaseAgent):
 
     def initialize_agent(self):
-        #This runs once before the bot starts up
-        self.kickoff = bt.BehaviourTree(bt.RepeatUntilFailure(task.GoTowards([datafetch.ball_location, False, True])))
-        self.behaviour = bt.BehaviourTree(
-            bt.Sequencer([
-                bt.RepeatUntilFailure(bt.Sequencer([
-                    # while
-                    bt.Selector([
-                        guard.DistanceLessThan([1000, datafetch.my_location, datafetch.ball_location]),
-                        # or
-                        guard.IsPointInZone([datafetch.ball_location, datafetch.my_half_zone])
-                    ]),
-                    # do
-                    task.PushBall()
-                ])),
-                bt.RepeatUntilFailure(bt.Sequencer([
-                    # while
-                    bt.Inverter(guard.DistanceLessThan([300, datafetch.my_location, datafetch.my_goal_location])),
-                    # do
-                    task.GoTowards([datafetch.my_goal_location, True, False])
-                ])),
-                task.FacePoint([datafetch.ball_location]),
-                bt.RepeatUntilFailure(bt.Sequencer([
-                    # while
-                    guard.IsPointInZone([datafetch.ball_location, datafetch.enemy_half_zone]),
-                    # do
-                    task.Wait()
-                ]))
-            ]))
-            
-        #task.GoTowards([datafetch.ball_location])
+
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         car = packet.game_cars[self.index]
         
-        if not packet.game_info.is_kickoff_pause:
-            # run kickoff behaviour
-            self.behaviour.reset()
-            return self.kickoff.resolve(car, packet)
-        else:
-            self.kickoff.reset()
-            return self.behaviour.resolve(car, packet)
+        return SimpleControllerState()
---
+++ beastbot/easing.py
@@ -0,0 +1,67 @@
+import math
+
+def linear(t):
+	return t
+
+def flip(t):
+	return 1-t
+
+def smooth_start(n, t):
+	return t**n
+
+def smooth_stop(n, t):
+	return 1-(1-t)**n
+
+def mix(c, A, B, t):
+	return c*A(t) + (1-c)*B(t)
+
+def crossfade(A, B, t):
+	return (1-t)*A(t) + t*B(t)
+
+def smooth_step(n, t):
+	return (1-t)*t**(n-1) + t*(1-(1-t)**(n-1))
+
+def arch(n, t):
+	return (t*(1-t))**n
+
+def arch_tall(n, t):
+	return arch(n, t)/arch(n, 0.5)
+
+def simple_bezier(t):
+	return -2*t*t*t + 3*t*t
+
+def bezier4(t):
+	u = 1-t
+	return t*t*t + 3*t*t*u
+
+def hesitate2(t):
+	return 2*t*(1-t)*(1-t) + t*t
+	
+def hesitate3(t):
+	return 3*t*(1-t)*(1-t) + t*t*t
+
+def inv_lerp(low, high, t):
+	if high-low == 0:
+		return 0
+	return (t-low)/(high-low)
+
+def to_01(low, high, t):
+	return inv_lerp(low, high, t)
+
+def lerp(low, high, t):
+	return low + t*(high-low)
+
+def from_01(high, low, t):
+	return lerp(low, high, t)
+
+def remap(prev_low, prev_high, new_low, new_high, t):
+	out = inv_lerp(prev_low, prev_high, t)
+	out = lerp(new_low, new_high, out)
+	return out
+
+def fix(t):
+	if t > 1:
+		return 1
+	if t < 0:
+		return 0
+	return 0
