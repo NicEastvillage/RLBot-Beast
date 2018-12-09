fac59c0a7b8230de4ff78aa663524946e4ccb104
NicEastvillage <nicoesterby@gmail.com>
2018-12-09 22:43:40 +0100

    Atba time.

--- beastbot/render.py
+++ beastbot/render.py
@@ -5,6 +5,12 @@ class FakeRenderer:
     def __init__(self):
         pass
 
+    def begin_rendering(self):
+        pass
+
+    def end_rendering(self):
+        pass
+
     def create_color(self, a, r, g, b):
         pass
 
@@ -19,3 +25,21 @@ class FakeRenderer:
 
     def draw_line_3d(self, start, end, color):
         pass
+
+    def draw_polyline_3d(self, vectors, color):
+        pass
+
+
+def draw_ball_path(bot, duration, step_size):
+    ball_prediction = bot.get_ball_prediction_struct()
+    if ball_prediction is not None and duration > 0 and step_size > 0:
+        time_passed = 0
+        steps_taken = 0
+        locations = [ball_prediction.slices[0].physics.location]
+        while time_passed < duration and steps_taken + step_size < ball_prediction.num_slices:
+            steps_taken += step_size
+            time_passed += step_size * 0.016666
+            locations.append(ball_prediction.slices[steps_taken].physics.location)
+
+        if steps_taken > 0:
+            bot.renderer.draw_polyline_3d(locations, bot.renderer.create_color(255, 255, 0, 0))
--- beastbot/rlmath.py
+++ beastbot/rlmath.py
@@ -1,23 +1,51 @@
 import math
 
+from RLUtilities.LinearAlgebra import *
 
-# returns sign of x, and 0 if x=0
-def sign(x):
+
+# returns sign of x, and 0 if x == 0
+def sign0(x) -> float:
     return x and (1, -1)[x < 0]
 
 
-def lerp(a, b, t):
+def sign(x) -> float:
+    return (1, -1)[x < 0]
+
+
+def lerp(a, b, t: float):
     return (1 - t) * a + t * b
 
 
-def inv_lerp(a, b, t):
-    return a if b - a == 0 else (t - a) / (b - a)
+def inv_lerp(a, b, v) -> float:
+    return a if b - a == 0 else (v - a) / (b - a)
 
 
-def fix_ang(ang):
+def fix_ang(ang: float) -> float:
     while abs(ang) > math.pi:
         if ang < 0:
             ang += 2 * math.pi
         else:
             ang -= 2 * math.pi
     return ang
+
+
+def proj_onto(src: vec3, dir: vec3) -> vec3:
+    try:
+        return (dot(src, dir) / dot(dir, dir)) * dir
+    except ZeroDivisionError:
+        return vec3()
+
+
+def proj_onto_size(src: vec3, dir: vec3) -> float:
+    try:
+        dir_n = normalize(dir)
+        return dot(src, dir_n) / dot(dir_n, dir_n)  # can be negative!
+    except ZeroDivisionError:
+        return norm(src)
+
+
+def rotated_2d(vec: vec3, ang: float) -> vec3:
+    c = math.cos(ang)
+    s = math.sin(ang)
+    return vec3(c * vec[0] - s * vec[1],
+                s * vec[0] + c * vec[1])
