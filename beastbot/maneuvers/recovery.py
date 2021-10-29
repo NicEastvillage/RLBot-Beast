1d178cd79a22a4d2d9b22341906bb810c3f8659b
NicEastvillage <nicoesterby@gmail.com>
2021-10-29 23:12:01 +0200

    Add PD recoveries and field geometry estimation using SDF

--- beastbot/maneuvers/recovery.py
+++ beastbot/maneuvers/recovery.py
@@ -1,31 +1,31 @@
 from maneuvers.aerialturn import AerialTurnManeuver
+from utility.field_sdf import sdf_contains, sdf_normal
 from utility.info import Car
+from utility.predict import DummyObject, fall
 from utility.vec import normalize, xy, Vec3, cross, Mat33, norm
 
 
 class RecoveryManeuver(AerialTurnManeuver):
     def __init__(self, bot):
-        super().__init__(RecoveryManeuver.find_landing_orientation(bot.info.my_car, 200))
+        super().__init__(RecoveryManeuver.find_landing_orientation(bot.info.my_car, 120))
+
+    def exec(self, bot):
+        self.target = RecoveryManeuver.find_landing_orientation(bot.info.my_car, 120)
+        return super(RecoveryManeuver, self).exec(bot)
 
     @staticmethod
     def find_landing_orientation(car: Car, num_points: int) -> Mat33:
-        """
         dummy = DummyObject(car)
-        trajectory = [Vec3(dummy.pos)]
 
         for i in range(0, num_points):
             fall(dummy, 0.0333)  # Apply physics and let car fall through the air
-            trajectory.append(Vec3(dummy.pos))
-            up = dummy.pitch_surface_normal()
-            if norm(up) > 0.0 and i > 10:
-                up = normalize(up)
-                forward = normalize(dummy.vel - dot(dummy.vel, up) * up)
-                left = cross(up, forward)
+
+            if i > 5 and sdf_contains(dummy.pos):
+                up = normalize(sdf_normal(dummy.pos))
+                left = cross(normalize(dummy.vel), up)
+                forward = cross(up, left)
 
                 return Mat33.from_columns(forward, left, up)
-
-        return Mat33(car.rot)
-        """
 
         forward = normalize(xy(car.vel)) if norm(xy(car.vel)) > 20 else car.forward
         up = Vec3(z=1)
